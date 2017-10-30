27638c5d2d2b6c9795c9e3267c655aa8c27f2fac
mazey <mazey@mazey.net>
2017-10-30 18:25:16 +0800

    获取地址栏参数 this.$route.params

--- src/components/apple.vue
+++ src/components/apple.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    apple
+    <h1>{{ msg }}</h1>
+    <button @click="getParam">get param</button>
   </div>
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'i am apple'
+      }
+    },
+    methods: {
+      getParam () {
+        console.log(this.$route.params)
+      }
+    }
+  }
+</script>
