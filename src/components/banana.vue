8f513be7044cd49115d5d04140b16361a590fc6f
mazey <mazey@mazey.net>
2017-11-01 12:53:11 +0800

    Vuex 使用示例

--- src/components/banana.vue
+++ src/components/banana.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    banana
+    <h1>{{ msg }}</h1>
+    <button @click="addOne">add one</button>
+    <button @click="minusOne">minus one</button>
   </div>
 </template>
+
+
+<script>
+  export default {
+    data () {
+      return {
+        msg: 'i am banana',
+        price: 15
+      }
+    },
+    methods: {
+      addOne () {
+        this.$store.commit('increment', this.price)
+      },
+      minusOne () {
+        this.$store.commit('decrement', this.price)
+      }
+    }
+  }
+</script>
