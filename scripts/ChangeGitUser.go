cc57824b93bb2f46f023277dd77bde28b6a56031
Mazey Chu <mazey@mazey.net>
2022-09-26 14:21:49 +0800

    feat: complete script

--- scripts/ChangeGitUser.go
+++ scripts/ChangeGitUser.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
 
 	"github.com/bitfield/script"
 )
@@ -9,7 +10,43 @@ import (
 func main() {
 	fmt.Println("I am scripts.")
 	// script.Exec("ping 127.0.0.1").Stdout()
-	script.FindFiles(".git").Stdout()
+	// https://bitfieldconsulting.com/golang/scripting
+	// cmdLines := "echo 233 && pwd"
+	// cmdLines += " && echo {{.}}"
+	// cmdLines += " && echo 22"
+	// cmdLines += " && echo ---"
+	// script.ListFiles("/Users/mazey/Web/Mazey/*/.git/config").ExecForEach(`/bin/sh -c echo "222222"; echo "333"; echo "444";`).Stdout()
+	script.ListFiles("/Users/mazey/Web/Mazey/*/.git").FilterLine(func(s string) string {
+		cmdLines := "echo " + s + " - begin -;"
+		cmdLines += "cd " + s + ";"
+		// cmdLines += "pwd;"
+		cmdLines += `git config user.email "mazey@mazey.net";`
+		cmdLines += `git config user.name "Mazey Chu";`
+		cmdLines += "echo - end -;"
+		cmd := exec.Command("/bin/sh", "-c", cmdLines)
+		result, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("err", err)
+		}
+		fmt.Printf("Output: %s", result)
+		// script.Exec("(echo 222 && echo 333)").Stdout()
+		// fmt.Println("print " + s)
+		// cmdLines1 := `git config user.email "mazey@mazey.net"`
+		// cmdLines2 := `git config user.name "Mazey Chu";`
+		// script.Exec("cd " + s).Stdout()
+		// script.Exec("pwd").Stdout()
+		// script.Exec(cmdLines1).Stdout()
+		// script.Exec(cmdLines2).Stdout()
+		return s
+	}).Stdout()
 	// script.FindFiles("*.go").Stdout()
 	// ExecForEach("gofmt -w {{ . }}")
+
+	// cmdSeq := "cd ~; pwd"
+	// cmd := exec.Command("/bin/sh", "-c", cmdSeq)
+	// result, err := cmd.CombinedOutput()
+	// if err != nil {
+	// 	fmt.Println("err", err)
+	// }
+	// fmt.Printf("Output: %s", result)
 }
