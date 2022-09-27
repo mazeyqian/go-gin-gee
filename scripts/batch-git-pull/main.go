2d1defc64fc4001f1a48fb03038194adc0a93666
Mazey Chu <mazey@mazey.net>
2022-09-27 20:17:37 +0800

    feat: add batch-git-pull script

---
+++ scripts/batch-git-pull/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+
+	"github.com/bitfield/script"
+)
+
+func main() {
+	fmt.Println("Change git user...")
+
+	script.ListFiles("/Users/mazey/Web/Example").FilterLine(func(s string) string {
+		cmdLines := "echo - begin -;"
+		cmdLines += fmt.Sprintf("echo Path: %s;", s)
+		cmdLines += fmt.Sprintf("cd %s;", s)
+		cmdLines += `git checkout master;`
+		cmdLines += `git pull;`
+		cmdLines += "echo - end -;"
+		cmd := exec.Command("/bin/sh", "-c", cmdLines)
+		result, err := cmd.CombinedOutput()
+		if err != nil {
+			fmt.Println("Err:", err)
+		}
+		fmt.Printf("%s", result)
+		return s
+	}).Stdout()
+}
