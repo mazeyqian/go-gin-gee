a424ce47c775901d3a1d4469d519a94ef61ba3a0
Mazey Chu <mazey@mazey.net>
2022-09-30 14:50:00 +0800

    feat: complete `transfer-notes-to-md-table`

--- scripts/transfer-notes-to-md-table/main.go
+++ scripts/transfer-notes-to-md-table/main.go
@@ -3,23 +3,28 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/bitfield/script"
 )
 
 func main() {
-	fmt.Println("Init...")
-
-	contents, _ := script.File("/Users/mazey/Web/Mazey/go-gin-gee/data/s.txt").String()
-
-	fmt.Println(contents)
-
 	fmt.Println("- begin -")
-
-	re := regexp.MustCompile("^.*\t.*((?!\t).)*$")
-
-	// script.File("/Users/mazey/Web/Mazey/go-gin-gee/data/s.txt").ExecForEach("echo -, {{.}}").Stdout()
-	script.File("/Users/mazey/Web/Mazey/go-gin-gee/data/s.txt").MatchRegexp(re).ExecForEach("echo -, {{.}}").Stdout()
-
+	re := regexp.MustCompile("^.*\t.*\t.*$")
+	script.File("/Users/mazey/Web/Mazey/go-gin-gee/data/s.txt").MatchRegexp(re).FilterLine(func(s string) string {
+		ss := strings.Split(s, "\t")
+		sss := ss[:3]
+		if sss[0] == "Title" || sss[0] == "" || len(sss) != 3 {
+			return "| - | - | - | # |"
+		}
+		retS := ""
+		for _, v := range sss {
+			ssss := fmt.Sprintf("| %s ", v)
+			retS += ssss
+		}
+		retS += "| # |"
+		s = retS
+		return s
+	}).WriteFile("./data/sw.txt")
 	fmt.Println("- end -")
 }
