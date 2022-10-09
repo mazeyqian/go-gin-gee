4addbf71c7f4eb41c8574141a3643480b4a5895e
Mazey Chu <mazey@mazey.net>
2022-10-09 20:19:36 +0800

    feat: complete restart main

---
+++ scripts/restart/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+)
+
+func main() {
+	fmt.Println("Restart...")
+
+	cmdLines := "echo - begin -;"
+	cmdLines += `cd /web/go-gin-gee;`
+	cmdLines += `git checkout master;`
+	cmdLines += `git pull;`
+	cmdLines += `GOOS=linux GOARCH=amd64 go build -o dist/api-linux-amd64 cmd/api/main.go;`
+	cmdLines += `supervisorctl restart api;`
+	cmdLines += "echo - end -;"
+	cmd := exec.Command("/bin/sh", "-c", cmdLines)
+	result, err := cmd.CombinedOutput()
+	if err != nil {
+		fmt.Println("Err:", err)
+	}
+	fmt.Printf("%s", result)
+}
