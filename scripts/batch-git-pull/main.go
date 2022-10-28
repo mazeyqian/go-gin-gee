be84231a06cdc5b3f8d651e8566a524875d6883c
Mazey Chu <mazey@mazey.net>
2022-10-28 19:07:54 +0800

    feat: git pull by paths

--- scripts/batch-git-pull/main.go
+++ scripts/batch-git-pull/main.go
@@ -2,27 +2,45 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
+	"regexp"
 
 	"github.com/bitfield/script"
 )
 
 func main() {
-	fmt.Println("Change git user...")
-
-	script.ListFiles("/Users/mazey/Web/Mazey").FilterLine(func(s string) string {
-		cmdLines := "echo - begin -;"
+	log.Println("Change git user...")
+	projectPath := "/Users/mazey/Web/Mazey"
+	projects := []string{
+		"go-gin-gee",
+		"json-to-resume",
+		"mazey",
+		"mazey-study",
+		"mazey-server",
+	}
+	regexStr := "^.+("
+	for _, v := range projects {
+		regexStr += fmt.Sprintf("%s|", v)
+	}
+	regexStr += "placeholder)$"
+	log.Println("regexStr:", regexStr)
+	regex := regexp.MustCompile(regexStr) // "^.+(json-to-resume|mazey-server)$")
+	script.ListFiles(projectPath).MatchRegexp(regex).FilterLine(func(s string) string {
+		cmdLines := "echo - - begin - -;"
+		cmdLines += "echo ;"
 		cmdLines += fmt.Sprintf("echo Path: %s;", s)
-		cmdLines += fmt.Sprintf("cd %s;", s)
-		cmdLines += `git checkout master;`
-		cmdLines += `git pull;`
-		cmdLines += "echo - end -;"
+		// cmdLines += fmt.Sprintf("cd %s;", s)
+		// cmdLines += `git checkout master;`
+		// cmdLines += `git pull;`
+		cmdLines += "echo ;"
+		cmdLines += "echo - - end - - - - - - - - - - - - - - - - -;"
 		cmd := exec.Command("/bin/sh", "-c", cmdLines)
 		result, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println("Err:", err)
+			log.Println("error:", err)
 		}
-		fmt.Printf("%s", result)
+		log.Printf("result: %s", result)
 		return s
 	}).Stdout()
 }
