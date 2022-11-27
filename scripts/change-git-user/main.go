75d4c26074026fcc8c967f1fee7dbdf667adc817
Mazey Chu <mazey@mazey.net>
2022-11-28 00:20:36 +0800

    feat: optimize `change` structure

--- scripts/change-git-user/main.go
+++ scripts/change-git-user/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os/exec"
 
 	"github.com/bitfield/script"
@@ -9,48 +10,32 @@ import (
 )
 
 func main() {
-	fmt.Println("Change git user...")
-	// script.Exec("ping 127.0.0.1").Stdout()
+	log.Println("Change git user...")
+	/*
+	 * Air Example: "/Volumes/ProjectX/Example"
+	 * "example@example.net" "Example Na"
+	 * Pro Mazey: "/Users/mazey/Web/Mazey"
+	 * "mazey@mazey.net" "Mazey Chu"
+	 */
+	absolutePath := "/Users/mazey/Web/Mazey"
+	userEmail := "mazey@mazey.net"
+	userName := "Mazey Chu"
 	// https://bitfieldconsulting.com/golang/scripting
-	// cmdLines := "echo 233 && pwd"
-	// cmdLines += " && echo {{.}}"
-	// cmdLines += " && echo 22"
-	// cmdLines += " && echo ---"
-	// script.ListFiles("/Users/mazey/Web/Mazey/*/.git/config").ExecForEach(`/bin/sh -c echo "222222"; echo "333"; echo "444";`).Stdout()
 	// https://pkg.go.dev/github.com/bitfield/script#ListFiles
-	script.ListFiles("/Users/mazey/Web/Mazey/*/.git").FilterLine(func(s string) string {
-		// cmdLines := "echo " + s + " - begin -;"
-		cmdLines := constants.ScriptStartMsg         // "echo - begin -;"
-		cmdLines += fmt.Sprintf("echo Path: %s;", s) // https://pkg.go.dev/fmt#Sprintf
-		cmdLines += fmt.Sprintf("cd %s;", s)         // "cd " + s + ";"
-		// cmdLines += "pwd;"
-		cmdLines += `git config user.email "mazey@mazey.net";`
-		cmdLines += `git config user.name "Mazey Chu";`
-		cmdLines += constants.ScriptEndMsg // "echo - end -;"
+	script.ListFiles(fmt.Sprintf("%s/*/.git", absolutePath)).FilterLine(func(s string) string {
+		cmdLines := constants.ScriptStartMsg
+		// https://pkg.go.dev/fmt#Sprintf
+		cmdLines += fmt.Sprintf("echo Path: %s;", s)
+		cmdLines += fmt.Sprintf("cd %s;", s)
+		cmdLines += fmt.Sprintf(`git config user.email "%s";`, userEmail)
+		cmdLines += fmt.Sprintf(`git config user.name "%s";`, userName)
+		cmdLines += constants.ScriptEndMsg
 		cmd := exec.Command("/bin/sh", "-c", cmdLines)
 		result, err := cmd.CombinedOutput()
 		if err != nil {
-			fmt.Println("Err:", err)
+			log.Println("error:", err)
 		}
-		fmt.Printf("%s", result)
-		// script.Exec("(echo 222 && echo 333)").Stdout()
-		// fmt.Println("print " + s)
-		// cmdLines1 := `git config user.email "mazey@mazey.net"`
-		// cmdLines2 := `git config user.name "Mazey Chu";`
-		// script.Exec("cd " + s).Stdout()
-		// script.Exec("pwd").Stdout()
-		// script.Exec(cmdLines1).Stdout()
-		// script.Exec(cmdLines2).Stdout()
-		return "- p -" // s
+		log.Printf("%s", result)
+		return ""
 	}).Stdout()
-	// script.FindFiles("*.go").Stdout()
-	// ExecForEach("gofmt -w {{ . }}")
-
-	// cmdSeq := "cd ~; pwd"
-	// cmd := exec.Command("/bin/sh", "-c", cmdSeq)
-	// result, err := cmd.CombinedOutput()
-	// if err != nil {
-	// 	fmt.Println("err", err)
-	// }
-	// fmt.Printf("Output: %s", result)
 }
