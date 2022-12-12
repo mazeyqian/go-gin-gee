09dedca19d685dfa33ad1becd265b2a741975b44
Mazey Chu <mazey@mazey.net>
2022-12-12 21:21:03 +0800

    feat: optimize filename

--- scripts/transfer-notes-to-md-table/main.go
+++ scripts/transfer-notes-to-md-table/main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	log.Println("- begin -")
 	re := regexp.MustCompile("^.*\t.*\t.*$")
-	script.File("/Users/mazey/Web/Mazey/go-gin-gee/data/s.txt").MatchRegexp(re).FilterLine(func(s string) string {
+	script.File("./data/s.txt").MatchRegexp(re).FilterLine(func(s string) string {
 		ss := strings.Split(s, "\t")
 		sss := ss[:3]
 		if sss[0] == "Title" || sss[0] == "" || len(sss) != 3 {
