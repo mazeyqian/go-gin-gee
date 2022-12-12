63b1a7131328cf8860c8006f81631853e8204336
Mazey Chu <mazey@mazey.net>
2022-12-12 21:25:08 +0800

    feat: create new script

---
+++ scripts/transfer-markdown-to-comments/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+	"strings"
+
+	"github.com/bitfield/script"
+	"github.com/mazeyqian/go-gin-gee/internal/pkg/constants"
+)
+
+func main() {
+	log.Println(constants.ScriptEndMsg)
+	re := regexp.MustCompile("^.*\t.*\t.*$")
+	script.File("./data/s.txt").MatchRegexp(re).FilterLine(func(s string) string {
+		ss := strings.Split(s, "\t")
+		sss := ss[:3]
+		if sss[0] == "Title" || sss[0] == "" || len(sss) != 3 {
+			return "| - | - | - | # |"
+		}
+		retS := ""
+		for _, v := range sss {
+			ssss := fmt.Sprintf("| %s ", v)
+			retS += ssss
+		}
+		retS += "| # |"
+		s = retS
+		return s
+	}).WriteFile("./data/sw.txt")
+	log.Println("- end -")
+}
