c75af6891b29175c5200c5fd328e4fccfc4ac37b
Mazey Chu <mazey@mazey.net>
2022-12-13 14:40:46 +0800

    style: remove unuseful comments

--- scripts/convert-markdown-to-comments/main.go
+++ scripts/convert-markdown-to-comments/main.go
@@ -14,21 +14,7 @@ func main() {
 	iFilename := fmt.Sprintf("./data/%s-for-typedoc.md", alias)
 	oFilename := fmt.Sprintf("./data/%s-for-typedoc.js", alias)
 	index := 0
-	// re := regexp.MustCompile("^.*\t.*\t.*$")
-	// script.File(filename).MatchRegexp(re).FilterLine(func(s string) string {
 	script.File(iFilename).FilterLine(func(s string) string {
-		// ss := strings.Split(s, "\t")
-		// sss := ss[:3]
-		// if sss[0] == "Title" || sss[0] == "" || len(sss) != 3 {
-		// 	return "| - | - | - | # |"
-		// }
-		// retS := ""
-		// for _, v := range sss {
-		// 	ssss := fmt.Sprintf("| %s ", v)
-		// 	retS += ssss
-		// }
-		// retS += "| # |"
-		// s = retS
 		retStr := s
 		if index == 0 {
 			retStr = "/**\n * " + retStr
