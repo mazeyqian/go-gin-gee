124d61e5acd1f0338730f86d0d124ed60d5d38be
Mazey Chu <mazey@mazey.net>
2023-02-09 19:57:46 +0800

    feat: use `lo.Find`

--- internal/pkg/models/docker/api.go
+++ internal/pkg/models/docker/api.go
@@ -2,38 +2,40 @@ package docker
 
 import "time"
 
+type DockerV2TagsResult struct {
+	Creator int `json:"creator"`
+	ID      int `json:"id"`
+	Images  []struct {
+		Architecture string      `json:"architecture"`
+		Features     string      `json:"features"`
+		Variant      interface{} `json:"variant"`
+		Digest       string      `json:"digest"`
+		Os           string      `json:"os"`
+		OsFeatures   string      `json:"os_features"`
+		OsVersion    interface{} `json:"os_version"`
+		Size         int         `json:"size"`
+		Status       string      `json:"status"`
+		LastPulled   time.Time   `json:"last_pulled"`
+		LastPushed   time.Time   `json:"last_pushed"`
+	} `json:"images"`
+	LastUpdated         time.Time `json:"last_updated"`
+	LastUpdater         int       `json:"last_updater"`
+	LastUpdaterUsername string    `json:"last_updater_username"`
+	Name                string    `json:"name"`
+	Repository          int       `json:"repository"`
+	FullSize            int       `json:"full_size"`
+	V2                  bool      `json:"v2"`
+	TagStatus           string    `json:"tag_status"`
+	TagLastPulled       time.Time `json:"tag_last_pulled"`
+	TagLastPushed       time.Time `json:"tag_last_pushed"`
+	MediaType           string    `json:"media_type"`
+	ContentType         string    `json:"content_type"`
+	Digest              string    `json:"digest"`
+}
+
 type DockerV2Tags struct {
-	Count    int         `json:"count"`
-	Next     interface{} `json:"next"`
-	Previous interface{} `json:"previous"`
-	Results  []struct {
-		Creator int `json:"creator"`
-		ID      int `json:"id"`
-		Images  []struct {
-			Architecture string      `json:"architecture"`
-			Features     string      `json:"features"`
-			Variant      interface{} `json:"variant"`
-			Digest       string      `json:"digest"`
-			Os           string      `json:"os"`
-			OsFeatures   string      `json:"os_features"`
-			OsVersion    interface{} `json:"os_version"`
-			Size         int         `json:"size"`
-			Status       string      `json:"status"`
-			LastPulled   time.Time   `json:"last_pulled"`
-			LastPushed   time.Time   `json:"last_pushed"`
-		} `json:"images"`
-		LastUpdated         time.Time `json:"last_updated"`
-		LastUpdater         int       `json:"last_updater"`
-		LastUpdaterUsername string    `json:"last_updater_username"`
-		Name                string    `json:"name"`
-		Repository          int       `json:"repository"`
-		FullSize            int       `json:"full_size"`
-		V2                  bool      `json:"v2"`
-		TagStatus           string    `json:"tag_status"`
-		TagLastPulled       time.Time `json:"tag_last_pulled"`
-		TagLastPushed       time.Time `json:"tag_last_pushed"`
-		MediaType           string    `json:"media_type"`
-		ContentType         string    `json:"content_type"`
-		Digest              string    `json:"digest"`
-	} `json:"results"`
+	Count    int                  `json:"count"`
+	Next     interface{}          `json:"next"`
+	Previous interface{}          `json:"previous"`
+	Results  []DockerV2TagsResult `json:"results"`
 }
--- internal/pkg/persistence/docker-repository.go
+++ internal/pkg/persistence/docker-repository.go
@@ -2,10 +2,10 @@ package persistence
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/go-resty/resty/v2"
 	models "github.com/mazeyqian/go-gin-gee/internal/pkg/models/docker"
+	"github.com/samber/lo"
 )
 
 type DockerRepository struct{}
@@ -28,14 +28,24 @@ func (d *DockerRepository) GetTagName(namespace string, repository string, inclu
 	// https://registry.hub.docker.com/v2/repositories/mazeyqian/go-gin-gee/tags?page_size=100
 	url := fmt.Sprintf("https://registry.hub.docker.com/v2/repositories/%s/%s/tags?page_size=20", namespace, repository)
 	client := resty.New()
-	resp, err := client.R().
+	_, err = client.R().
 		SetResult(dockerV2Tags).
 		Get(url)
 	if err != nil {
 		return tagName, err
 	}
-	log.Println("  Body       :\n", resp)
-	log.Println("dockerV2Tags:", dockerV2Tags)
-	log.Println("dockerV2Tags.Results:", dockerV2Tags.Results)
+	// log.Println("  Body       :\n", resp)
+	// log.Println("dockerV2Tags:", dockerV2Tags)
+	// log.Println("dockerV2Tags.Results:", dockerV2Tags.Results)
+	findNames := lo.Find[*models.DockerV2TagsResult](dockerV2Tags.Results, func(v models.DockerV2TagsResult) bool {
+		// log.Println("lo.Substring(v.Name, -3, 3)", lo.Substring(v.Name, -3, 3))
+		// log.Println("includedStr", includedStr)
+		// log.Println("lo.Substring(v.Name, -3, 3) == includedStr", lo.Substring(v.Name, -3, 3) == includedStr)
+		return lo.Substring(v.Name, -3, 3) == includedStr
+	})
+	// log.Println("findNames:", findNames)
+	// log.Println("findNames len:", len(findNames))
+	// log.Println("findNames[0]:", findNames[0])
+	// log.Println("findNames[0] Name:", findNames[0].Name)
 	return tagName, err
 }
