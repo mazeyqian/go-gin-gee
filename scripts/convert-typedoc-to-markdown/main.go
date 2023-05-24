3f5264d27aebb313f3072fa9718092ae4dc3890c
Mazey Chu <mazey@mazey.net>
2023-05-24 20:10:50 +0800

    feat: convert typedoc comments to markdown

--- scripts/convert-typedoc-to-markdown/main.go
+++ scripts/convert-typedoc-to-markdown/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 	"strings"
 
@@ -54,12 +55,14 @@ func main() {
 	// td2md.js td2md.md
 	iFilename := fmt.Sprintf("./data/%s.js", alias)
 	oFilename := fmt.Sprintf("./data/%s.md", alias)
-	index := 0
+	// index := 0
 	// hasZH := false
 	// https://gobyexample.com/regular-expressions
 	// rMatchZH, _ := regexp.Compile("^<!-- (.+) -->$")
 	// rMatchCommentBoth, _ := regexp.Compile("^<!-- (.+) -->$")
 	rMatchCommentStart, _ := regexp.Compile(`^ *\*?\s?(.*)$`)
+	rMatchCommentStartEN, _ := regexp.Compile(`^ *\*?\s?EN:\s(.*)$`)
+	placeholderChar := "#loading..."
 	// rMatchCommentEnd, _ := regexp.Compile("^(.+) -->$")
 	// zhStartStr := " * ZH:"
 	// Determine the intel evironment.
@@ -71,20 +74,31 @@ func main() {
 	// 	return constants.RunningMsg
 	// }).Stdout()
 	// Add comments.
-	script.File(iFilename).FilterLine(func(s string) string {
+	// Filter not #p
+	// error parsing regexp: invalid or unsupported Perl syntax: `(?!`
+	// re := regexp.MustCompile(`^(?!#p)`)
+	// EN:
+	// ZH:
+	script.File(iFilename).Match(" *").FilterLine(func(s string) string {
 		retStr := s
 		if strings.Contains(s, "/**") {
-			log.Println("First Line: ", retStr)
-			retStr = ""
+			// log.Println("First Line: ", retStr)
+			retStr = placeholderChar // "#p"
 		} else if strings.Contains(s, " */") {
-			log.Println("Last Line: ", retStr)
-			retStr = ""
+			// log.Println("Last Line: ", retStr)
+			retStr = placeholderChar // "#p"
 		} else if strings.Contains(s, " * @") {
-			log.Println("Ignore Line: ", retStr)
-			retStr = ""
+			// log.Println("Ignore Line: ", retStr)
+			retStr = placeholderChar // "#p"
+		} else if strings.Contains(s, " * ZH:") {
+			// log.Println("Ignore Line: ", retStr)
+			retStr = placeholderChar // "#p"
+		} else if strings.Contains(s, " * EN:") {
+			// log.Println("Ignore Line: ", retStr)
+			retStr = rMatchCommentStartEN.FindStringSubmatch(retStr)[1]
 		} else if strings.Contains(s, " *") || strings.Contains(s, " * ") {
 			retStr = rMatchCommentStart.FindStringSubmatch(retStr)[1]
-			log.Println("Normal Line: ", retStr)
+			// log.Println("Normal Line: ", retStr)
 		} else {
 			log.Println("Error: ", retStr)
 		}
@@ -109,12 +123,35 @@ func main() {
 		// 		retStr = " * " + retStr
 		// 	}
 		// }
-		index++
-		// log.Println(retStr)
+		// index++
+		log.Println(retStr)
 		return retStr
-	}).WriteFile(oFilename)
+		// Filter #p
+	}).Reject(placeholderChar).WriteFile(oFilename)
 	// https://pkg.go.dev/github.com/bitfield/script#Pipe.AppendFile
 	// script.Echo(endLine).AppendFile(oFilename)
 	// log.Println(endLine)
 	log.Println(constants.EndMsgStr)
+
+	// Read the output file and remove extra empty lines
+	re2 := regexp.MustCompile(`\n{3,}`)
+	re3 := regexp.MustCompile(`\n+$`)
+	output, err := os.ReadFile(oFilename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+	outputStr := string(output)
+	outputStr = re2.ReplaceAllString(outputStr, "\n\n")
+	outputStr = re3.ReplaceAllString(outputStr, "")
+	output = []byte(outputStr)
+
+	// Write the trimmed output back to the output file
+	err = os.WriteFile(oFilename, output, 0644)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("All done.")
 }
