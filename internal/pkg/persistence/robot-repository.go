a34b4b7993131e1df26b73028b86e10e73577d42
Mazey Chu <mazey@mazey.net>
2023-05-29 11:46:53 +0800

    feat: Health Check data should be an array
    Close #33

--- internal/pkg/persistence/robot-repository.go
+++ internal/pkg/persistence/robot-repository.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"time"
 
 	"github.com/go-resty/resty/v2"
@@ -56,30 +57,47 @@ func (s *Sites) getWebSiteStatus() (*[]SiteStatus, *[]SiteStatus, error) {
 
 func (s *Sites) ClearCheckResult() (*wxworkbot.Markdown, error) {
 	ss := s
-	ss.List = map[string]SiteStatus{
-		"https://i.mazey.net/cdn/jquery-2.1.1.min.js": {"CDN jQuery", 200},
-	}
-	ss.List["https://blog.mazey.net/"] = SiteStatus{"Blog Home", 200}
-	ss.List[fmt.Sprintf("%s%d", "https://blog.mazey.net/?s=", time.Now().Unix())] = SiteStatus{"Blog Search", 200}
-	ss.List["https://feperf.com/api/mazeychu/413fbc97-4bed-3228-9f07-9141ba07c9f3"] = SiteStatus{"Gee Gin NameId0920", 200}
-	ss.List["https://i.mazey.net/tool/markdown/"] = SiteStatus{"Markdown Converter", 200}
-	ss.List["https://mazey.cn/feperf/monitor/get/topic?userName=%E5%90%8E%E9%99%A4"] = SiteStatus{"FE Perf Monitor Topic", 200}
-	ss.List["https://mazey.cn/server/nut/feeds?currentPage=1&pageSize=10&total=0&isPrivacy=1"] = SiteStatus{"Nut Read Feeds", 200}
-	ss.List["https://mazey.cn/t/k"] = SiteStatus{"Tiny Redirect", 200}
-	ss.List["https://www.zhibaifa.com/"] = SiteStatus{"Treat White Home", 200}
-	ss.List["https://mazey.cn/server/weather/new-daily?location=shanghai"] = SiteStatus{"Weather Shanghai", 200}
-	ss.List["https://mazey.cn/server/user/info"] = SiteStatus{"Location", 200}
-	ss.List["https://i.mazey.net/cdn/bootstrap-3.4.1/css/bootstrap.min.css"] = SiteStatus{"Bootstrap CSS", 200}
-	ss.List["https://mazey.cn/cdn/layer/layer.js"] = SiteStatus{"Layer", 200}
-	ss.List["https://mazey.cn/cdn/jquery-2.1.1.min.js"] = SiteStatus{"jQuery", 200}
+	ss.List = map[string]SiteStatus{}
+	// ss.List = map[string]SiteStatus{
+	// 	"https://i.mazey.net/cdn/jquery-2.1.1.min.js": {"CDN jQuery", 200},
+	// }
+	// List - begin
+	// CDN
+	ss.List["https://i.mazey.net/cdn/jquery-2.1.1.min.js"] = SiteStatus{"CDN/Net/Arc/jQuery", 200}
+	ss.List["https://i.mazey.net/cdn/bootstrap-3.4.1/css/bootstrap.min.css"] = SiteStatus{"CDN/Net/Arc/Bootstrap CSS", 200}
+	ss.List["https://mazey.cn/cdn/layer/layer.js"] = SiteStatus{"CDN/Cn/Layer", 200}
+	ss.List["https://mazey.cn/cdn/jquery-2.1.1.min.js"] = SiteStatus{"CDN/Cn/jQuery", 200}
+	// Website
+	ss.List["https://blog.mazey.net/"] = SiteStatus{"Blog/Home", 200}
+	ss.List[fmt.Sprintf("%s%d", "https://blog.mazey.net/?s=", time.Now().Unix())] = SiteStatus{"Blog/Search", 200}
+	ss.List["https://www.zhibaifa.com/"] = SiteStatus{"White/Home", 200}
+	ss.List["https://i.mazey.net/tool/markdown/"] = SiteStatus{"Tool/Arc/Markdown Converter", 200}
+	// Api
+	ss.List["https://mazey.cn/feperf/monitor/get/topic?userName=%E5%90%8E%E9%99%A4"] = SiteStatus{"Server/Monitor Topic", 200}
+	ss.List["https://mazey.cn/server/nut/feeds?currentPage=1&pageSize=10&total=0&isPrivacy=1"] = SiteStatus{"Server/Nut Read Feeds", 200}
+	ss.List["https://mazey.cn/t/k"] = SiteStatus{"Server/Tiny Redirect", 200}
+	ss.List["https://mazey.cn/server/weather/new-daily?location=shanghai"] = SiteStatus{"Server/Weather Shanghai", 200}
+	ss.List["https://mazey.cn/server/user/info"] = SiteStatus{"Server/Location", 200}
+	ss.List["https://feperf.com/api/mazeychu/413fbc97-4bed-3228-9f07-9141ba07c9f3"] = SiteStatus{"Gee/Chu/NameId0920", 200}
+	// List - end
 	healthySites, failSites, err := ss.getWebSiteStatus()
 	if err != nil {
 		log.Println("error:", err)
 	}
+	sucessNames := []string{}
+	lo.ForEach(*healthySites, func(site SiteStatus, _ int) {
+		sucessNames = append(sucessNames, site.Name)
+	})
+	// Sort sucessNames
+	sort.Strings(sucessNames)
+	log.Println("sucessNames:", sucessNames)
 	mdStr := "Health Check Result:\n"
-	for _, site := range *healthySites {
-		mdStr += fmt.Sprintf("<font color=\"info\">%s OK</font>\n", site.Name)
-	}
+	// for _, site := range *healthySites {
+	// 	mdStr += fmt.Sprintf("<font color=\"info\">%s OK</font>\n", site.Name)
+	// }
+	lo.ForEach(sucessNames, func(name string, _ int) {
+		mdStr += fmt.Sprintf("<font color=\"info\">%s OK</font>\n", name)
+	})
 	lo.ForEach(*failSites, func(site SiteStatus, _ int) {
 		// log.Println("ForEach SiteStatus:", site.Name)
 		siteLink, _ := lo.FindKeyBy(ss.List, func(k string, v SiteStatus) bool {
