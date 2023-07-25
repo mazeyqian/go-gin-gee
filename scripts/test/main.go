a53149021c6c59a487945233a2990a5c78dd92d2
Mazey Chu <mazey@mazey.net>
2023-07-25 18:43:38 +0800

    test: time

--- scripts/test/main.go
+++ scripts/test/main.go
@@ -2,59 +2,14 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"time"
 )
 
-func ShanghaiToUTC(shanghaiTime string) (string, error) {
-	shanghaiHour, err := strconv.Atoi(shanghaiTime[:2])
-	if err != nil {
-		return "", err
-	}
-	shanghaiMinute, err := strconv.Atoi(shanghaiTime[3:])
-	if err != nil {
-		return "", err
-	}
-	shanghaiTotalMinutes := shanghaiHour*60 + shanghaiMinute
-	utcTotalMinutes := (shanghaiTotalMinutes - 480 + 1440) % 1440
-	utcHour := utcTotalMinutes / 60
-	utcMinute := utcTotalMinutes % 60
-	utcTime := fmt.Sprintf("%02d:%02d", utcHour, utcMinute)
-	return utcTime, nil
-}
-
 func main() {
-	shanghaiTime := "10:05"
-	utcTime, err := ShanghaiToUTC(shanghaiTime)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s in Shanghai is %s in UTC\n", shanghaiTime, utcTime)
-
-	shanghaiTime = "04:01"
-	utcTime, err = ShanghaiToUTC(shanghaiTime)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s in Shanghai is %s in UTC\n", shanghaiTime, utcTime)
-
-	shanghaiTime = "02:59"
-	utcTime, err = ShanghaiToUTC(shanghaiTime)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s in Shanghai is %s in UTC\n", shanghaiTime, utcTime)
-
-	shanghaiTime = "00:05"
-	utcTime, err = ShanghaiToUTC(shanghaiTime)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s in Shanghai is %s in UTC\n", shanghaiTime, utcTime)
-
-	shanghaiTime = "00:65"
-	utcTime, err = ShanghaiToUTC(shanghaiTime)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("%s in Shanghai is %s in UTC\n", shanghaiTime, utcTime)
+	timestamp := int64(1686830930000)
+	seconds := timestamp / 1000
+	nanos := (timestamp % 1000) * 1000000
+	t := time.Unix(seconds, nanos)
+	hour := t.Format("15")
+	fmt.Println(hour)
 }
