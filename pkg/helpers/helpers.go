1562fff726d1de7e2f76259c5f1041d8a66a949d
Mazey Chu <mazey@mazey.net>
2023-09-25 14:29:26 +0800

    style(helpers): remove surplus function

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -3,7 +3,6 @@ package helpers
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"fmt"
 	"regexp"
 	"strconv"
 	"strings"
@@ -46,21 +45,4 @@ func ToSnakeCase(str string) string {
 func ConvertStringToMD5Hash(text string) string {
 	hash := md5.Sum([]byte(text))
 	return hex.EncodeToString(hash[:])
-}
-
-func ConvertShanghaiToUTC(shanghaiTime string) (string, error) {
-	shanghaiHour, err := strconv.Atoi(shanghaiTime[:2])
-	if err != nil {
-		return "", err
-	}
-	shanghaiMinute, err := strconv.Atoi(shanghaiTime[3:])
-	if err != nil {
-		return "", err
-	}
-	shanghaiTotalMinutes := shanghaiHour*60 + shanghaiMinute
-	utcTotalMinutes := (shanghaiTotalMinutes - 480 + 1440) % 1440
-	utcHour := utcTotalMinutes / 60
-	utcMinute := utcTotalMinutes % 60
-	utcTime := fmt.Sprintf("%02d:%02d", utcHour, utcMinute)
-	return utcTime, nil
 }
