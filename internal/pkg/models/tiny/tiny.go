a010b34d90cb7ab537420e0ac4d95672ed4ec77c
Cheng <mazeyqian@gmail.com>
2024-01-31 12:11:33 +0800

    feat: keep track
    Keeps track of how many times the short URL has been visited.

--- internal/pkg/models/tiny/tiny.go
+++ internal/pkg/models/tiny/tiny.go
@@ -8,11 +8,12 @@ import (
 
 type Tiny struct {
 	models.Model
-	OriLink  string `gorm:"column:ori_link;not null;size:350" json:"ori_link" form:"ori_link"`
-	OriMd5   string `gorm:"column:ori_md5;not null;size:40" json:"ori_md5" form:"ori_md5"`
-	TinyLink string `gorm:"column:tiny_link;not null;size:30" json:"tiny_link" form:"tiny_link"`
-	TinyKey  string `gorm:"column:tiny_key;not null;size:20" json:"tiny_key" form:"tiny_key"`
-	OneTime  bool   `gorm:"column:one_time;not null;default:false" json:"one_time" form:"one_time"`
+	OriLink    string `gorm:"column:ori_link;not null;size:350" json:"ori_link" form:"ori_link"`
+	OriMd5     string `gorm:"column:ori_md5;not null;size:40" json:"ori_md5" form:"ori_md5"`
+	TinyLink   string `gorm:"column:tiny_link;not null;size:30" json:"tiny_link" form:"tiny_link"`
+	TinyKey    string `gorm:"column:tiny_key;not null;size:20" json:"tiny_key" form:"tiny_key"`
+	OneTime    bool   `gorm:"column:one_time;not null;default:false" json:"one_time" form:"one_time"`
+	VisitCount int    `gorm:"column:visit_count;not null;default:0" json:"visit_count" form:"visit_count"`
 }
 
 func (m *Tiny) BeforeCreate() error {
