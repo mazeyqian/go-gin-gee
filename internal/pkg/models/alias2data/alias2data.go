e1754dadc1133c734d80bb5613e0534b04dcabbb
Cheng <mazeyqian@gmail.com>
2024-07-25 11:22:19 +0800

    feat(alias): set size

--- internal/pkg/models/alias2data/alias2data.go
+++ internal/pkg/models/alias2data/alias2data.go
@@ -8,10 +8,10 @@ import (
 
 type Alias2data struct {
 	models.Model
-	Alias  string `gorm:"column:alias;not null;" json:"alias" form:"alias"`
-	Data   string `gorm:"column:data;not null;" json:"data" form:"data"`
-	Type   string `gorm:"column:type;not null;" json:"type" form:"type"`
-	Public bool   `gorm:"column:public;not null;" json:"public" form:"public"`
+	Alias  string `gorm:"column:alias;not null;size:50" json:"alias" form:"alias"`
+	Data   string `gorm:"column:data;not null;size:255" json:"data" form:"data"`
+	Type   string `gorm:"column:type;not null;size:20" json:"type" form:"type"`
+	Public bool   `gorm:"column:public;not null;default:true" json:"public" form:"public"`
 }
 
 func (m *Alias2data) BeforeCreate() error {
--- internal/pkg/models/sites/sites.go
+++ internal/pkg/models/sites/sites.go
@@ -1,7 +1,7 @@
 package models
 
 type WebSite struct {
-	Name string `gorm:"column:name;not null;" json:"name"`
-	Link string `gorm:"column:link;not null;" json:"link"`
-	Code int    `gorm:"column:code;not null;" json:"code"`
+	Name string `gorm:"column:name;not null;size:50" json:"name"`
+	Link string `gorm:"column:link;not null;size:255" json:"link"`
+	Code int    `gorm:"column:code;not null;default:200" json:"code"`
 }
